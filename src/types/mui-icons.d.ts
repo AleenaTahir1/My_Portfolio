3f64de875c5ab935f086b3defb6a1b8b96a728e6
Razee4315 <saqlainrazee@gmail.com>
2025-08-08 03:21:53 +0500

    Deply issue fix

---
+++ src/types/mui-icons.d.ts
@@ -0,0 +1,8 @@
+declare module '@mui/icons-material/*' {
+  import React from 'react'
+  import { SvgIconProps } from '@mui/material/SvgIcon'
+  const IconComponent: React.ComponentType<SvgIconProps>
+  export default IconComponent
+}
+
+
